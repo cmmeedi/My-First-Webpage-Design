fbc0eee78d2eaea67362bdd8b5a16651557e4795
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2022-04-13 19:15:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-            <div id="musicImage" class="producImage">
-                <img src="" alt="">
-            </div>
+<?php
 
-            <div id="musicSummary" class="productSummary">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam temporibus, earum dolorum quis aperiam totam minus est! Iusto repellendus quibusdam neque consectetur dolorem dicta doloribus? Ipsum, sint? Eos, impedit necessitatibus?
-                </p>
-            </div>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form method="get" action="/Media-Queries/Pricing-Panel/index.html">
+        <button style="margin: auto;">Click Me</button>
+    </form>
+    
+</body>
+</html>
